ed6c11157dda969bf9e2190f7cef972360086e54
updaun <updauney@daum.net>
2025-02-21 16:49:11 +0900

    chore: custom exception handler, json response

--- Exception_Handler/start/main.py
+++ Exception_Handler/start/main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Request
 from fastapi.staticfiles import StaticFiles
 from routes import blog
 from contextlib import asynccontextmanager
 from db.database import engine
+from fastapi.responses import JSONResponse
 
 
 @asynccontextmanager
@@ -20,3 +21,15 @@ app = FastAPI(lifespan=lifespan)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(blog.router)
+
+
+@app.exception_handler(HTTPException)
+async def custom_http_exception_handler(request: Request, exc: HTTPException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={
+            "error": "처리 중 에러가 발생하였습니다.",
+            "detail": exc.detail,
+            "code": exc.status_code,
+        },
+    )
