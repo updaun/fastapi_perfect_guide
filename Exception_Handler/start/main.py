6d852bd1cde879a0af6ad330d20f5f0b4f2c2a05
updaun <updauney@daum.net>
2025-02-22 17:23:56 +0900

    chore: custom exception handler, template response

--- Exception_Handler/start/main.py
+++ Exception_Handler/start/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.staticfiles import StaticFiles
-from routes import blog
+
 from contextlib import asynccontextmanager
 from db.database import engine
 from fastapi.responses import JSONResponse
+from fastapi.templating import Jinja2Templates
 
 
 @asynccontextmanager
@@ -19,17 +20,31 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+templates = Jinja2Templates(directory="templates")
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+from routes import blog
+
 app.include_router(blog.router)
 
 
 @app.exception_handler(HTTPException)
 async def custom_http_exception_handler(request: Request, exc: HTTPException):
-    return JSONResponse(
-        status_code=exc.status_code,
-        content={
-            "error": "처리 중 에러가 발생하였습니다.",
+    # return JSONResponse(
+    #     status_code=exc.status_code,
+    #     content={
+    #         "error": "처리 중 에러가 발생하였습니다.",
+    #         "detail": exc.detail,
+    #         "code": exc.status_code,
+    #     },
+    # )
+    return templates.TemplateResponse(
+        request=request,
+        name="http_error.html",
+        context={
+            "status_code": exc.status_code,
+            "title_message": "불편을 드려 죄송합니다",
             "detail": exc.detail,
-            "code": exc.status_code,
         },
     )
