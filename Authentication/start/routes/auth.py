fddc22ba4f984affcc3c7521a79355509ec91fe9
updaun <updauney@daum.net>
2025-03-03 19:19:03 +0900

    chore: email duplicate process

--- Authentication/start/routes/auth.py
+++ Authentication/start/routes/auth.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Request, Depends, Form, UploadFile, File, status
 from fastapi.responses import RedirectResponse, JSONResponse
+from fastapi.exceptions import HTTPException
 from fastapi.templating import Jinja2Templates
 from db.database import context_get_conn
 from sqlalchemy import Connection
@@ -37,11 +38,17 @@ async def register_uesr(
     password: str = Form(min_length=2, max_length=30),
     conn: Connection = Depends(context_get_conn),
 ):
+    user = await auth_svc.get_user_by_email(conn=conn, email=email)
+    if user is not None:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="이미 등록된 사용자입니다. 다른 이메일을 사용해주세요.",
+        )
     hashed_password = get_hashed_password(password)
     await auth_svc.register_user(
         conn=conn, name=name, email=email, hashed_password=hashed_password
     )
-    return
+    return RedirectResponse("/blogs", status_code=status.HTTP_302_FOUND)
 
 
 @router.get("/login")
--- Authentication/start/services/auth_svc.py
+++ Authentication/start/services/auth_svc.py
@@ -2,13 +2,47 @@ from fastapi import status, UploadFile
 from fastapi.exceptions import HTTPException
 from sqlalchemy import text, Connection
 from sqlalchemy.exc import SQLAlchemyError
-from schemas.blog_schema import BlogData
+from schemas.auth_schema import UserData
 from utils import util
 from typing import List
 from dotenv import load_dotenv
 import os
 import time
 import aiofiles as aio
+
+
+async def get_user_by_email(conn: Connection, email: str):
+    try:
+        query = f"""
+        SELECT id, name, email  from user
+        where email = :email
+        """
+        stmt = text(query)
+        bind_stmt = stmt.bindparams(email=email)
+        result = await conn.execute(bind_stmt)
+        # 만약에 한건도 찾지 못하면 오류를 던진다.
+        if result.rowcount == 0:
+            return None
+
+        row = result.fetchone()
+
+        user = UserData(id=row[0], name=row[1], email=row[2])
+
+        result.close()
+        return user
+
+    except SQLAlchemyError as e:
+        print(e)
+        raise HTTPException(
+            status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+            detail="요청하신 서비스가 잠시 내부적으로 문제가 발생하였습니다.",
+        )
+    except Exception as e:
+        print(e)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="알수없는 이유로 서비스 오류가 발생하였습니다",
+        )
 
 
 async def register_user(conn: Connection, name: str, email: str, hashed_password: str):
