ed289039af01b21c7adad057ae3bab493bf6239b
updaun <updauney@daum.net>
2025-03-09 22:12:36 +0900

    chore: blog join user

--- Signed_Cookie/start/schemas/blog_schema.py
+++ Signed_Cookie/start/schemas/blog_schema.py
@@ -3,22 +3,26 @@ from datetime import datetime
 from typing import Optional, Annotated
 from pydantic.dataclasses import dataclass
 
+
 class BlogInput(BaseModel):
     title: str = Field(..., min_length=2, max_length=200)
     author: str = Field(..., max_length=100)
     content: str = Field(..., min_length=2, max_length=4000)
     image_loc: Optional[str] = Field(None, max_length=400)
-    #image_loc: Annotated[str, Field(None, max_length=400)] = None
+    # image_loc: Annotated[str, Field(None, max_length=400)] = None
+
 
 class Blog(BlogInput):
     id: int
     modified_dt: datetime
 
-@dataclass
-class BlogData:
+
+class BlogData(BaseModel):
     id: int
     title: str
-    author: str
+    author_id: int
+    author: str | None = None
+    email: str | None = None
     content: str
     modified_dt: datetime
     image_loc: str | None = None
--- Signed_Cookie/start/services/blog_svc.py
+++ Signed_Cookie/start/services/blog_svc.py
@@ -17,63 +17,92 @@ UPLOAD_DIR = os.getenv("UPLOAD_DIR")
 async def get_all_blogs(conn: Connection) -> List:
     try:
         query = """
-        SELECT id, title, author, content, 
+        SELECT a.id, title, author_id, b.name as author, b.email as email, content, 
         case when image_loc is null then '/static/default/blog_default.png'
              else image_loc end as image_loc
-        , modified_dt FROM blog;
+        , modified_dt 
+        FROM blog a
+          join user b on a.author_id = b.id
         """
         result = await conn.execute(text(query))
-        all_blogs = [BlogData(id=row.id,
-              title=row.title,
-              author=row.author,
-              content=util.truncate_text(row.content),
-              image_loc=row.image_loc, 
-              modified_dt=row.modified_dt) for row in result]
-    
+        all_blogs = [
+            BlogData(
+                id=row.id,
+                title=row.title,
+                author_id=row.author_id,
+                author=row.author,
+                email=row.email,
+                content=util.truncate_text(row.content),
+                image_loc=row.image_loc,
+                modified_dt=row.modified_dt,
+            )
+            for row in result
+        ]
+
         result.close()
         return all_blogs
     except SQLAlchemyError as e:
         print(e)
-        raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
-                            detail="요청하신 서비스가 잠시 내부적으로 문제가 발생하였습니다.")
+        raise HTTPException(
+            status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+            detail="요청하신 서비스가 잠시 내부적으로 문제가 발생하였습니다.",
+        )
     except Exception as e:
         print(e)
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                            detail="알수없는 이유로 서비스 오류가 발생하였습니다")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="알수없는 이유로 서비스 오류가 발생하였습니다",
+        )
 
 
 async def get_blog_by_id(conn: Connection, id: int):
     try:
         query = f"""
-        SELECT id, title, author, content, image_loc, modified_dt from blog
-        where id = :id
+        SELECT a.id, title, author_id, b.name as author, b.email as email, content, image_loc, modified_dt 
+        from blog a
+            join user b on a.author_id = b.id
+        where a.id = :id
         """
         stmt = text(query)
         bind_stmt = stmt.bindparams(id=id)
         result = await conn.execute(bind_stmt)
-        # 만약에 한건도 찾지 못하면 오류를 던진다. 
+        # 만약에 한건도 찾지 못하면 오류를 던진다.
         if result.rowcount == 0:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                                detail=f"해당 id {id}는(은) 존재하지 않습니다.")
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"해당 id {id}는(은) 존재하지 않습니다.",
+            )
 
         row = result.fetchone()
-        blog = BlogData(id=row[0], title=row[1], author=row[2], 
-                        content=row[3],
-                        image_loc=row[4], modified_dt=row[5])
+        blog = BlogData(
+            id=row.id,
+            title=row.title,
+            author_id=row.author_id,
+            author=row.author,
+            email=row.email,
+            content=row.content,
+            image_loc=row.image_loc,
+            modified_dt=row.modified_dt,
+        )
         if blog.image_loc is None:
-            blog.image_loc = '/static/default/blog_default.png'
-        
+            blog.image_loc = "/static/default/blog_default.png"
+
         result.close()
         return blog
-    
+
     except SQLAlchemyError as e:
         print(e)
-        raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
-                            detail="요청하신 서비스가 잠시 내부적으로 문제가 발생하였습니다.")
+        raise HTTPException(
+            status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+            detail="요청하신 서비스가 잠시 내부적으로 문제가 발생하였습니다.",
+        )
     except Exception as e:
         print(e)
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                            detail="알수없는 이유로 서비스 오류가 발생하였습니다")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="알수없는 이유로 서비스 오류가 발생하였습니다",
+        )
+
 
 async def upload_file(author: str, imagefile: UploadFile = None):
     try:
@@ -91,37 +120,45 @@ async def upload_file(author: str, imagefile: UploadFile = None):
         print("upload succeeded:", upload_image_loc)
 
         return upload_image_loc[1:]
-    
+
     except Exception as e:
         print(e)
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                            detail="이미지 파일이 제대로 Upload되지 않았습니다. ")
-  
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="이미지 파일이 제대로 Upload되지 않았습니다. ",
+        )
 
-async def create_blog(conn: Connection, title:str, author: str, 
-                content:str, image_loc = None):
+
+async def create_blog(
+    conn: Connection, title: str, author: str, content: str, image_loc=None
+):
     try:
         query = f"""
         INSERT INTO blog(title, author, content, image_loc, modified_dt)
         values ('{title}', '{author}', '{content}', {util.none_to_null(image_loc, is_squote=True)} , now())
         """
-        
+
         await conn.execute(text(query))
         await conn.commit()
-        
+
     except SQLAlchemyError as e:
         print(e)
         await conn.rollback()
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-                            detail="요청데이터가 제대로 전달되지 않았습니다.")
-    
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="요청데이터가 제대로 전달되지 않았습니다.",
+        )
 
-async def update_blog(conn: Connection,  id: int
-                ,  title: str
-                , author: str
-                , content: str
-                , image_loc: str = None):
-    
+
+async def update_blog(
+    conn: Connection,
+    id: int,
+    title: str,
+    author: str,
+    content: str,
+    image_loc: str = None,
+):
+
     try:
         query = f"""
         UPDATE blog 
@@ -129,22 +166,26 @@ async def update_blog(conn: Connection,  id: int
         , image_loc = :image_loc
         where id = :id
         """
-        bind_stmt = text(query).bindparams(id=id, title=title, 
-                                           author=author, content=content
-                                           , image_loc=image_loc)
+        bind_stmt = text(query).bindparams(
+            id=id, title=title, author=author, content=content, image_loc=image_loc
+        )
         result = await conn.execute(bind_stmt)
         # 해당 id로 데이터가 존재하지 않아 update 건수가 없으면 오류를 던진다.
         if result.rowcount == 0:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                                detail=f"해당 id {id}는(은) 존재하지 않습니다.")
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"해당 id {id}는(은) 존재하지 않습니다.",
+            )
         await conn.commit()
-        
+
     except SQLAlchemyError as e:
         print(e)
         await conn.rollback()
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-                            detail="요청데이터가 제대로 전달되지 않았습니다. ")
-    
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="요청데이터가 제대로 전달되지 않았습니다. ",
+        )
+
 
 async def delete_blog(conn: Connection, id: int, image_loc: str = None):
     try:
@@ -157,8 +198,10 @@ async def delete_blog(conn: Connection, id: int, image_loc: str = None):
         result = await conn.execute(bind_stmt)
         # 해당 id로 데이터가 존재하지 않아 delete 건수가 없으면 오류를 던진다.
         if result.rowcount == 0:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                                detail=f"해당 id {id}는(은) 존재하지 않습니다.")
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"해당 id {id}는(은) 존재하지 않습니다.",
+            )
         await conn.commit()
 
         if image_loc is not None:
@@ -170,11 +213,14 @@ async def delete_blog(conn: Connection, id: int, image_loc: str = None):
     except SQLAlchemyError as e:
         print(e)
         await conn.rollback()
-        raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
-                            detail="요청하신 서비스가 잠시 내부적으로 문제가 발생하였습니다.")
+        raise HTTPException(
+            status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+            detail="요청하신 서비스가 잠시 내부적으로 문제가 발생하였습니다.",
+        )
     except Exception as e:
         print(e)
         await conn.rollback()
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                            detail="알수없는 이유로 문제가 발생하였습니다. ")
-
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="알수없는 이유로 문제가 발생하였습니다. ",
+        )
